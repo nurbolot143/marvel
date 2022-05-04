aa9f4d417197b21dcad5fa286a589bce3211e87a
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-05-04 23:16:07 +0300

    Сonnected random character

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 
 import "./style/style.scss";
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharahters().then((res) => console.log(res.data.results));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,16 +12,30 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharahters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=147&${this._apiKey}`
     );
+
+    return res.data.results.map(this._tranformCharacter);
   };
 
-  getCharahter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const res = await this.getResource(
       `${this._apiBase}characters/${id}?limit=9&offset=147&${this._apiKey}`
     );
+
+    return this._tranformCharacter(res.data.results[0]);
+  };
+
+  _tranformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
